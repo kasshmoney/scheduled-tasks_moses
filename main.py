7dd720caf83b3d40841d54931ebcb2d54a7df027
kasshmoney <kassehinjunior@yahoo.com>
2026-05-22 16:06:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import random
+import osimport random
 from datetime import datetime
 import pandas
 import smtplib
 
-MY_EMAIL = "juniorkassehinjk@gmail.com"
-MY_PASSWORD = "ubvojsunuurcbbzk"
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 
 today = datetime.now()
